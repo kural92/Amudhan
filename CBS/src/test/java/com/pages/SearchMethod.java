a8224e9a955e75072b8972c19f362e5c824d451a
kural92 <kuralamudhan92@gmail.com>
2020-03-25 10:31:18 +0530

    Search Module Update

--- CBS/src/test/java/com/pages/SearchMethod.java
+++ CBS/src/test/java/com/pages/SearchMethod.java
@@ -1,5 +1,256 @@
 package com.pages;
 
-public class SearchMethod {
+import base.BaseTest;
+import pom.cmlogin.Search_POM;
+
+public class SearchMethod extends BaseTest {
+	
+	
+	public static void regularSearch() throws Throwable {
+		
+		Search_POM s = new Search_POM(driver);
+		
+		Thread.sleep(2000);
+		BaseTest.click(s.getSearch_Menu_btn());
+		
+
+		BaseTest.select_text(s.getRegular_AgeFrom_DD(), BaseTest.getExcelData("Regular Search", 1, 1));
+		BaseTest.select_text(s.getRegular_AgeTo_DD(), BaseTest.getExcelData("Regular Search", 1, 2));
+		
+		BaseTest.select_text(s.getRegular_HeightFrom_DD(), BaseTest.getExcelData("Regular Search", 1, 3));
+		BaseTest.select_text(s.getRegular_HeightTo_DD(), BaseTest.getExcelData("Regular Search", 1, 4));
+		
+//////////////////////////////////////////////////////////////////////////
+		
+////////////////////////////////////Marital Status   ////////////////////////////////////		
+Thread.sleep(2000);
+if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Unmarried")) {
+if(!s.getRegular_Marital_Unmarried().isSelected()) {
+BaseTest.click(s.getRegular_Marital_Unmarried());
+} else {
+System.out.println("Unmarried Marital is Already Selected");
+}
+
+} else {}
+if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Widow / Widower")) {
+if(!s.getRegular_Marital_Widow().isSelected()) {
+BaseTest.click(s.getRegular_Marital_Widow());
+} else {
+System.out.println("Widow / Widower Marital is Already Selected");
+}
+
+} else {} 
+if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Divorced")) {
+if(!s.getRegular_Marital_Divorceed().isSelected()) {
+BaseTest.click(s.getRegular_Marital_Divorceed());
+} else {
+System.out.println("Divorced Marital is Already Selected");
+}
+
+} else {} 
+if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Separated")) {
+if(!s.getRegular_Marital_Seperated().isSelected()) {
+BaseTest.click(s.getRegular_Marital_Seperated());
+} else {
+System.out.println("Seperated Marital is Already Selected");
+}
+
+} else {}
+if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Any")) {
+if(!s.getRegular_Marital_Any().isSelected()) {
+BaseTest.click(s.getRegular_Marital_Any());
+} else {
+System.out.println("Any Marital is Already Selected");
+}
+}	
+/////////////////////////////////////////   *  *  *  *  *   ///////////////////////////////	
+/////////     Have Children  //////
+if (!s.getRegular_Marital_Unmarried().isSelected()||s.getRegular_Marital_Divorceed().isSelected()||s.getRegular_Marital_Seperated().isSelected()||s.getRegular_Marital_Widow().isSelected()||s.getRegular_Marital_Any().isSelected()) {
+if (BaseTest.getExcelData("Regular Search", 1, 6).equals("Doesn't matter")) {
+if(!s.getRegular_HaveChild__Doesnt().isSelected()) {
+BaseTest.click(s.getRegular_HaveChild__Doesnt());
+} else {
+System.out.println("Doesn't Matter Children Status is Already Selected");
+}
+
+} else if (BaseTest.getExcelData("Regular Search", 1, 6).equals("Yes. Living together")) {
+if(!s.getRegular_HaveChild_YesLiving().isSelected()) {
+BaseTest.click(s.getRegular_HaveChild_YesLiving());
+} else {
+System.out.println("Yes Living Together Children Status is Already Selected");
+}
+} else if (BaseTest.getExcelData("Regular Search", 1, 6).equals("Yes. Not living together")) {
+if(!s.getRegular_HaveChild_Yes_Not_Living().isSelected()) {
+BaseTest.click(s.getRegular_HaveChild_Yes_Not_Living());
+} else {
+System.out.println("Yes, Not Living Together Children Status is Already Selected");
+}
+} else {
+if(!s.getRegular_HaveChild_No().isSelected()) {
+BaseTest.click(s.getRegular_HaveChild_No());
+} else {
+System.out.println("No Children Status is Already Selected");
+}
+}
+} else {
+System.out.println("Marital Status is UnMarried - So there is no Children Status to Display/Select");
+}
+
+/////////////////////////////////////////////////////////////////////////////////	
+               //       Sub Caste   ///// 
+
+/*String SubCaste = BaseTest.getExcelData("Regular Search", 1, 7);
+Thread.sleep(2000);
+BaseTest.click(s.getRegular_Subcaste_Value());
+if (SubCaste.contains(", ")) {
+	String[] Subcaste = SubCaste.split(", ");
+	for (int j = 0; j < Subcaste.length; j++) {
+		
+		Thread.sleep(1000);
+		acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).build().perform();
+		//acc.moveToElement(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).doubleClick().build().perform();
+		Thread.sleep(1000);
+	} }else{
+		Thread.sleep(2000);
+		BaseTest.click(s.getRegular_Subcaste_Value());
+		acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+SubCaste+"')]"))).build().perform();
+	}
+	*/
+  /////////////////////////////////////////////////////////////////////////////////////
+
+ /* String MotherTongue = BaseTest.getExcelData("Regular Search", 1, 8);
+  
+  Thread.sleep(2000);
+	BaseTest.click(s.getRegular_MOtherTongue_Value());
+  
+		if (MotherTongue.contains(", ")) {
+			String[] Mother_tongue = MotherTongue.split(", ");
+			for (int j = 0; j < Mother_tongue.length; j++) {
+				
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).build().perform();
+				//acc.moveToElement(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).doubleClick().build().perform();
+				Thread.sleep(1000);
+			} }else{
+				Thread.sleep(2000);
+				BaseTest.click(s.getRegular_MOtherTongue_Value());
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+SubCaste+"')]"))).build().perform();
+			}  */
+/////////////////////////////////////////////////////////////////////////////////////////////////
+	/*	String Countryliving = BaseTest.getExcelData("Regular Search", 1, 9);
+		Thread.sleep(2000);
+		BaseTest.click(s.getRegular_CountryLiving_Value());
+		
+		if (Countryliving.contains(", ")) {
+			String[] Country_Living = Countryliving.split(", ");
+			for (int j = 0; j < Country_Living.length; j++) {
+				
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).build().perform();
+				//acc.moveToElement(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).doubleClick().build().perform();
+				Thread.sleep(1000);
+			} }else{
+				Thread.sleep(2000);
+				BaseTest.click(s.getRegular_CountryLiving_Value());
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+SubCaste+"')]"))).build().perform();
+			} */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+	/*	String Education = BaseTest.getExcelData("Regular Search", 1, 10);
+		Thread.sleep(2000);
+		BaseTest.click(s.getRegular_Education_Value());  
+		
+		if (Education.contains(", ")) {
+			String[] Education_detail = Education.split(", ");
+			for (int j = 0; j < Education_detail.length; j++) {
+				
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).build().perform();
+				//acc.moveToElement(driver.findElement(By.xpath("//option[contains(text(),'"+j+"')]"))).doubleClick().build().perform();
+				Thread.sleep(1000);
+			} }else{
+				Thread.sleep(2000);
+				BaseTest.click(s.getRegular_Education_Value());
+				Thread.sleep(1000);
+				acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+SubCaste+"')]"))).build().perform();
+			}
+		*/
+////////////////////////////////////////////////////////////////////////////////////////////
+		
+		String ShowProfile = BaseTest.getExcelData("Regular Search", 1, 11);
+		
+		if (ShowProfile.contains("Photo")) {
+			if (!s.getRegular_ShowProfile_Photo().isSelected()) {
+				BaseTest.click(s.getRegular_ShowProfile_Photo());
+			}else {
+				System.out.println("Show Profile With Photo was Already Selected");
+			}
+		} else { }
+		if (ShowProfile.contains("Horoscope")) {
+			if (!s.getRegular_ShowProfile_Horoscope().isSelected()) {
+				BaseTest.click(s.getRegular_ShowProfile_Horoscope());
+			}else {
+				System.out.println("Show Profile With Horoscopw was Already Selected");
+			}
+		} else { }
+		
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		String DontShow = BaseTest.getExcelData("Regular Search", 1, 12);
+		if (DontShow.contains("Contacted")) {
+			if (!s.getRegular_DontShow_Contacted().isSelected()) {
+				BaseTest.click(s.getRegular_DontShow_Contacted());
+			}else {
+				System.out.println("Dont Show Already Contacted Profile was Already Selected");
+			}
+		} else { }
+		
+		if (DontShow.contains("Viewed")) {
+			if (!s.getAdvanced_DontShow_Viewed().isSelected()) {
+				BaseTest.click(s.getAdvanced_DontShow_Viewed());
+			}else {
+				System.out.println("Dont Show Already Viewed Profile was Already Selected");
+			}
+		} else { }
+		
+		if (DontShow.contains("Shortlisted")) {
+			if (!s.getAdvanced_DontShow_ShortListed().isSelected()) {
+				BaseTest.click(s.getAdvanced_DontShow_ShortListed());
+			}else {
+				System.out.println("Dont Show Already Shortlisted Profile was Already Selected");
+			}
+		} else { }
+		
+		if (DontShow.contains("Blocked")) {
+			if (!s.getAdvanced_DontShow_Blocked().isSelected()) {
+				BaseTest.click(s.getAdvanced_DontShow_Blocked());
+			}else {
+				System.out.println("Dont Show Already Blocked Profile was Already Selected");
+			}
+		} else { }
+		
+		
+		if (DontShow.contains("Ignored")) {
+			if (!s.getAdvanced_DontShow_Ignored().isSelected()) {
+				BaseTest.click(s.getAdvanced_DontShow_Ignored());
+			}else {
+				System.out.println("Dont Show Already Ignored Profile was Already Selected");
+			}
+		} else { }
+		
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		BaseTest.click(s.getRegular_SEARCH_Btn());
+		
+		
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+			
+		
+		
+	}
 
 }
--- CBS/src/test/java/com/search/Search.java
+++ CBS/src/test/java/com/search/Search.java
@@ -13,42 +13,39 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.pages.EditPIMethod;
+import com.pages.SearchMethod;
 
 import base.BaseTest;
 import pom.cmlogin.EditPI_POM;
 import pom.cmlogin.GothramPOM;
 import pom.cmlogin.Search_POM;
 
-public class Search extends BaseTest{
-	
+public class Search extends BaseTest {
 
 	@BeforeSuite
 	public void setUp() {
 		// test=reports.startTest("Divorcee Negative Payment");
 		// test.log(LogStatus.PASS, "Divorcee Negative Payment Test Started");
-		System.setProperty("webdriver.chrome.driver",
-				".//Driver//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", ".//Driver//chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
-		//ChromeOptions options = new ChromeOptions();
-	//	options.addArguments("--disable-notifications");
-	//	options.addArguments("--disable-notifications");
+		// ChromeOptions options = new ChromeOptions();
+		// options.addArguments("--disable-notifications");
+		// options.addArguments("--disable-notifications");
 		driver = new ChromeDriver(options);
-	
+
 	}
-	
-	
+
 	@Test
 	public void regularSearch() throws Throwable {
-		
+
 		GothramPOM g = new GothramPOM(driver);
 		EditPI_POM i = new EditPI_POM(driver);
 		Actions acc = new Actions(driver);
 		Robot r = new Robot();
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		JavascriptExecutor js  = (JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		com.pages.EditPIMethod pp = new EditPIMethod();
-		
-		
+
 		driver.get("https://www.communitymatrimony.com");
 		driver.manage().window().maximize();
 		BaseTest.click(g.getMatriID());
@@ -56,22 +53,19 @@ public class Search extends BaseTest{
 		BaseTest.click(g.getPasswordClear());
 		BaseTest.typeData(g.getPassword(), "cbstest1");
 		BaseTest.click(g.getLogin_btn());
-		
-		
-		
-		
+
 		try {
 			BaseTest.click(g.getInterMediate_page());
 		} catch (Exception e2) {
 			// TODO: handle exception
 		}
-		
+
 		try {
 			BaseTest.click(g.getInterMediate_page1());
 		} catch (Exception e2) {
 			// TODO: handle exception
 		}
-		
+
 		Thread.sleep(5000);
 		try {
 			Alert al = driver.switchTo().alert();
@@ -79,112 +73,144 @@ public class Search extends BaseTest{
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		
+
 		Thread.sleep(5000);
 		try {
 			BaseTest.click(driver.findElement(By.xpath("//*[@id=\"special_offer_lightpic1\"]/div[1]/div/a/img")));
 		} catch (Exception e2) {
 			// TODO: handle exception
 		}
-		
+
 		Search_POM s = new Search_POM(driver);
-		
+
+		// SearchMethod.regularSearch();
+
+		/////////////////////////////////////////////
+
 		Thread.sleep(2000);
 		BaseTest.click(s.getSearch_Menu_btn());
+
+		try {
+			Thread.sleep(5000);
+			acc.moveToElement(driver.findElement(By.xpath("//*[@id=\"RSearchForm\"]/div[2]/div[1]/a[2]"))).build().perform();
+			driver.findElement(By.id("chatbuddylistwin")).click();
+			acc.moveToElement(driver.findElement(By.xpath("//*[@id=\"RSearchForm\"]/div[2]/div[1]/a[2]"))).click().build().perform();;
+		} catch (Exception e) {
+
+		}
+
+		
 		
 
-		BaseTest.select_text(s.getRegular_AgeFrom_DD(), BaseTest.getExcelData("Regular Search", 1, 1));
-		BaseTest.select_text(s.getRegular_AgeTo_DD(), BaseTest.getExcelData("Regular Search", 1, 2));
-		
-		BaseTest.select_text(s.getRegular_HeightFrom_DD(), BaseTest.getExcelData("Regular Search", 1, 3));
-		BaseTest.select_text(s.getRegular_HeightTo_DD(), BaseTest.getExcelData("Regular Search", 1, 4));
-		
+		Thread.sleep(2000);
+
+		BaseTest.select_text(s.getAdvanced_AgeFrom_DD(), BaseTest.getExcelData("Advanced Search", 1, 1));
+		BaseTest.select_text(s.getAdvanced_AgeTo_DD(), BaseTest.getExcelData("Advanced Search", 1, 2));
+
+		BaseTest.select_text(s.getAdvanced_HeightFrom_DD(), BaseTest.getExcelData("Advanced Search", 1, 3));
+		BaseTest.select_text(s.getAdvanced_HeightTo_DD(), BaseTest.getExcelData("Advanced Search", 1, 4));
+
 //////////////////////////////////////////////////////////////////////////
-		
+
 ////////////////////////////////////Marital Status   ////////////////////////////////////		
-Thread.sleep(2000);
-if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Unmarried")) {
-if(!s.getRegular_Marital_Unmarried().isSelected()) {
-BaseTest.click(s.getRegular_Marital_Unmarried());
-} else {
-System.out.println("Unmarried Marital is Already Selected");
-}
+		Thread.sleep(2000);
+		if (BaseTest.getExcelData("Advanced Search", 1, 5).contains("Unmarried")) {
+			if (!s.getAdvanced_Marital_Unmarried().isSelected()) {
+				BaseTest.click(s.getAdvanced_Marital_Unmarried());
+			} else {
+				System.out.println("Unmarried Marital is Already Selected");
+			}
 
-} else {}
-if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Widow / Widower")) {
-if(!s.getRegular_Marital_Widow().isSelected()) {
-BaseTest.click(s.getRegular_Marital_Widow());
-} else {
-System.out.println("Widow / Widower Marital is Already Selected");
-}
+		} else {
+		}
+		if (BaseTest.getExcelData("Advanced Search", 1, 5).contains("Widow / Widower")) {
+			if (!s.getAdvanced_Marital_Widow().isSelected()) {
+				BaseTest.click(s.getAdvanced_Marital_Widow());
+			} else {
+				System.out.println("Widow / Widower Marital is Already Selected");
+			}
 
-} else {} 
-if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Divorced")) {
-if(!s.getRegular_Marital_Divorceed().isSelected()) {
-BaseTest.click(s.getRegular_Marital_Divorceed());
-} else {
-System.out.println("Divorced Marital is Already Selected");
-}
+		} else {
+		}
+		if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Divorced")) {
+			if (!s.getAdvanced_Marital_Divorceed().isSelected()) {
+				BaseTest.click(s.getAdvanced_Marital_Divorceed());
+			} else {
+				System.out.println("Divorced Marital is Already Selected");
+			}
 
-} else {} 
-if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Separated")) {
-if(!s.getRegular_Marital_Seperated().isSelected()) {
-BaseTest.click(s.getRegular_Marital_Seperated());
-} else {
-System.out.println("Seperated Marital is Already Selected");
-}
+		} else {
+		}
+		if (BaseTest.getExcelData("Advanced Search", 1, 5).contains("Separated")) {
+			if (!s.getAdvanced_Marital_Seperated().isSelected()) {
+				BaseTest.click(s.getAdvanced_Marital_Seperated());
+			} else {
+				System.out.println("Seperated Marital is Already Selected");
+			}
 
-} else {}
-if (BaseTest.getExcelData("Regular Search", 1, 5).contains("Any")) {
-if(!s.getRegular_Marital_Any().isSelected()) {
-BaseTest.click(s.getRegular_Marital_Any());
-} else {
-System.out.println("Any Marital is Already Selected");
-}
-}	
+		} else {
+		}
+		if (BaseTest.getExcelData("Advanced Search", 1, 5).contains("Any")) {
+			if (!s.getAdvanced_Marital_Any().isSelected()) {
+				BaseTest.click(s.getRegular_Marital_Any());
+			} else {
+				System.out.println("Any Marital is Already Selected");
+			}
+		}
 /////////////////////////////////////////   *  *  *  *  *   ///////////////////////////////	
 /////////     Have Children  //////
-if (!s.getRegular_Marital_Unmarried().isSelected()||s.getRegular_Marital_Divorceed().isSelected()||s.getRegular_Marital_Seperated().isSelected()||s.getRegular_Marital_Widow().isSelected()||s.getRegular_Marital_Any().isSelected()) {
-if (BaseTest.getExcelData("EditPP", 1, 2).equals("Doesn't matter")) {
-if(!s.getRegular_HaveChild__Doesnt().isSelected()) {
-BaseTest.click(s.getRegular_HaveChild__Doesnt());
-} else {
-System.out.println("Doesn't Matter Children Status is Already Selected");
-}
+		if (!s.getAdvanced_Marital_Unmarried().isSelected() || s.getAdvanced_Marital_Divorceed().isSelected()
+				|| s.getAdvanced_Marital_Seperated().isSelected() || s.getAdvanced_Marital_Widow().isSelected()
+				|| s.getAdvanced_Marital_Any().isSelected()) {
+			if (BaseTest.getExcelData("Advanced Search", 1, 6).equals("Doesn't matter")) {
+				if (!s.getAdvanced_HaveChild__Doesnt().isSelected()) {
+					BaseTest.click(s.getAdvanced_HaveChild__Doesnt());
+				} else {
+					System.out.println("Doesn't Matter Children Status is Already Selected");
+				}
 
-} else if (BaseTest.getExcelData("EditPP", 1, 2).equals("Yes. Living together")) {
-if(!s.getRegular_HaveChild_YesLiving().isSelected()) {
-BaseTest.click(s.getRegular_HaveChild_YesLiving());
-} else {
-System.out.println("Yes Living Together Children Status is Already Selected");
-}
-} else if (BaseTest.getExcelData("EditPP", 1, 2).equals("Yes. Not living together")) {
-if(!s.getRegular_HaveChild_Yes_Not_Living().isSelected()) {
-BaseTest.click(s.getRegular_HaveChild_Yes_Not_Living());
-} else {
-System.out.println("Yes, Not Living Together Children Status is Already Selected");
-}
-} else {
-if(!s.getRegular_HaveChild_No().isSelected()) {
-BaseTest.click(s.getRegular_HaveChild_No());
-} else {
-System.out.println("No Children Status is Already Selected");
-}
-}
-} else {
-System.out.println("Marital Status is UnMarried - So there is no Children Status to Display/Select");
-}
+			} else if (BaseTest.getExcelData("Advanced Search", 1, 6).equals("Yes. Living together")) {
+				if (!s.getAdvanced_HaveChild_YesLiving().isSelected()) {
+					BaseTest.click(s.getAdvanced_HaveChild_YesLiving());
+				} else {
+					System.out.println("Yes Living Together Children Status is Already Selected");
+				}
+			} else if (BaseTest.getExcelData("Advanced Search", 1, 6).equals("Yes. Not living together")) {
+				if (!s.getAdvanced_HaveChild_Yes_Not_Living().isSelected()) {
+					BaseTest.click(s.getAdvanced_HaveChild_Yes_Not_Living());
+				} else {
+					System.out.println("Yes, Not Living Together Children Status is Already Selected");
+				}
+			} else {
+				if (!s.getAdvanced_HaveChild_No().isSelected()) {
+					BaseTest.click(s.getAdvanced_HaveChild_No());
+				} else {
+					System.out.println("No Children Status is Already Selected");
+				}
+			}
+		} else {
+			System.out.println("Marital Status is UnMarried - So there is no Children Status to Display/Select");
+		}
 
 /////////////////////////////////////////////////////////////////////////////////	
+		// Sub Caste /////
 
-		
-		
-		
-		
-		
+		/*
+		 * String SubCaste = BaseTest.getExcelData("Advanced Search", 1, 7);
+		 * Thread.sleep(2000); BaseTest.click(s.getRegular_Subcaste_Value()); if
+		 * (SubCaste.contains(", ")) { String[] Subcaste = SubCaste.split(", "); for
+		 * (int j = 0; j < Subcaste.length; j++) {
+		 * 
+		 * Thread.sleep(1000);
+		 * acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+j+
+		 * "')]"))).build().perform();
+		 * //acc.moveToElement(driver.findElement(By.xpath("//option[contains(text(),'"+
+		 * j+"')]"))).doubleClick().build().perform(); Thread.sleep(1000); } }else{
+		 * Thread.sleep(2000); BaseTest.click(s.getRegular_Subcaste_Value());
+		 * acc.doubleClick(driver.findElement(By.xpath("//option[contains(text(),'"+
+		 * SubCaste+"')]"))).build().perform(); }
+		 */
+
 	}
-
-	
 
 }
